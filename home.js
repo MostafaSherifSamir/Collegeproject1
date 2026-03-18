ea4530e501769499bdb1cb6b53c5fd3f47fc267c
Omar Ahmed <omarahmedgodabmox@gmail.com>
2026-03-18 14:57:59 +0200

    v1

---
+++ home.js
@@ -0,0 +1,36 @@
+// modal 
+let sideBar = document.querySelector("#sideBar");
+let modalOverlay = document.querySelector(".modal-screen");
+let appear = () => {
+  modalOverlay.style.transform = "scale(1)";
+  modalOverlay.style.opacity = "1";
+  sideBar.style.left = 0;
+  modalOverlay.style.pointerEvents = "all";
+  document.body.style.overflow = "hidden";
+};
+let disappear = () => {
+  sideBar.style.left = "-250px";
+  modalOverlay.style.opacity = "0";
+  modalOverlay.style.pointerEvents = "none";
+  document.body.style.overflow = "auto";
+  setTimeout(() => {
+    modalOverlay.style.transform = "scale(0)";
+  }, 500);
+};
+modalOverlay.addEventListener("click", (e) => {
+  if (e.target == modalOverlay) {
+    disappear();
+  }
+});
+
+// slider-hero section
+let index = 0;
+let slider = document.querySelector('.slider');
+let slides = document.querySelectorAll('.slide');
+
+let slide = (direction) => {
+    index += direction;
+    if (index < 0) index = slides.length - 1;
+    if (index >= slides.length) index = 0;
+    slider.style.transform = `translateX(-${index * 100}%)`;
+}
